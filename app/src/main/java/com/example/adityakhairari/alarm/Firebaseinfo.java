dc228797d3f6fc71ce7d9ec15c1e91e83ca7318e
peterchien <peterc3@illinois.edu>
2018-12-09 20:14:47 -0600

    Initial commit part 2: Put Alarm function and Multiple Choice question functions together-- 2 activities not yet connected

---
+++ app/src/main/java/com/example/adityakhairari/alarm/Firebaseinfo.java
@@ -0,0 +1,16 @@
+package com.example.adityakhairari.alarm;
+
+public class Firebaseinfo {
+    String question;
+
+    public Firebaseinfo(String q) {
+        question = q;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+    public void setQuestion(String q) {
+        question = q;
+    }
+}
---
+++ app/src/main/java/com/example/adityakhairari/alarm/SecondActivity.java
@@ -0,0 +1,169 @@
+package com.example.adityakhairari.alarm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class SecondActivity extends AppCompatActivity {
+    private TextView question;
+    private Button a;
+    private Button b;
+    private Button c;
+    private Button d;
+    private final String TAG = "MainActivity";
+    private String currentQuestion;
+    private String correctAnswer;
+    private int numberOfQuestions = 5;
+    private  boolean bool = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference myRef = database.getReference();
+
+        question = (TextView) findViewById(R.id.tvQuestion);
+        a = (Button) findViewById(R.id.btna);
+        b = (Button) findViewById(R.id.btnb);
+        c = (Button) findViewById(R.id.btnc);
+        d = (Button) findViewById(R.id.btnd);
+
+        a.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (a.getText().equals(correctAnswer)) {
+                    question.setText("CORRECT");
+                } else {
+                    question.setText("WRONG");
+                    //myRef.child("update").setValue(!bool);
+                }
+            }
+        });
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (b.getText().equals(correctAnswer)) {
+                    question.setText("CORRECT");
+                } else {
+                    question.setText("WRONG");
+                }
+            }
+        });
+        c.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (c.getText().equals(correctAnswer)) {
+                    question.setText("CORRECT");
+                } else {
+                    question.setText("WRONG");
+                }
+            }
+        });
+        d.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (d.getText().equals(correctAnswer)) {
+                    question.setText("CORRECT");
+                } else {
+                    question.setText("WRONG");
+                }
+            }
+        });
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String questionNumber = getRandomQuestion();
+                currentQuestion = questionNumber;
+
+                //everytime a randome position is generated, it is added to this list so that that pos. isn't used again
+                List<String> questionPositions = new ArrayList<>();
+
+                question.setText(String.valueOf(dataSnapshot.child(questionNumber).child("question").getValue()));
+
+                String first = randomPlacement();
+                questionPositions.add(first);
+                a.setText(String.valueOf(dataSnapshot.child(questionNumber).child(first).getValue()));
+                if (first.equals("correct_answer")) {
+                    correctAnswer = String.valueOf(dataSnapshot.child(questionNumber).child(first).getValue());
+                }
+
+                String second = randomPlacement();
+                while (questionPositions.contains(second)) {
+                    second = randomPlacement();
+                }
+                b.setText(String.valueOf(dataSnapshot.child(questionNumber).child(second).getValue()));
+                questionPositions.add(second);
+                if (second.equals("correct_answer")) {
+                    correctAnswer = String.valueOf(dataSnapshot.child(questionNumber).child(second).getValue());
+                }
+
+                String third = randomPlacement();
+                while (questionPositions.contains(third)) {
+                    third = randomPlacement();
+                }
+                c.setText(String.valueOf(dataSnapshot.child(questionNumber).child(third).getValue()));
+                questionPositions.add(third);
+                if (third.equals("correct_answer")) {
+                    correctAnswer = String.valueOf(dataSnapshot.child(questionNumber).child(third).getValue());
+                }
+
+                String fourth = randomPlacement();
+                while (questionPositions.contains(fourth)) {
+                    fourth = randomPlacement();
+                }
+                d.setText(String.valueOf(dataSnapshot.child(questionNumber).child(fourth).getValue()));
+                if (fourth.equals("correct_answer")) {
+                    correctAnswer = String.valueOf(dataSnapshot.child(questionNumber).child(fourth).getValue());
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+    }
+    public String getRandomQuestion() {
+        Random rand = new Random();
+        int n = rand.nextInt(numberOfQuestions) + 1;
+        return "question_" + n;
+    }
+
+    //places questions in random order
+    public String randomPlacement() {
+        Random rand = new Random();
+        int n = rand.nextInt(4) + 1;
+        if (n == 4) {
+            return "A";
+        } else if (n == 3) {
+            return "B";
+        } else if (n == 2) {
+            return "C";
+        } else {
+            return "correct_answer";
+        }
+
+    }
+}
